837e939204e8a4846b247e7c4098f99ba37e5956
Andrew <fakmaki99@mail.ru>
2021-05-09 00:17:42 +0500

    Photo removed

--- src/core/Message.java
+++ src/core/Message.java
@@ -5,68 +5,43 @@ import data.user.UserDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Message
-{
+public class Message {
     public String text;
     public Id id;
-    private String photoPath;
     public List<String> buttons;
 
-    public Message(String text, Id id)
-    {
+    public Message(String text, Id id) {
         this.text = text;
         this.id = id;
     }
 
-    public Message(String text, Id id, ArrayList<String> buttons)
-    {
+    public Message(String text, Id id, ArrayList<String> buttons) {
         this.text = text;
         this.id = id;
         this.buttons = buttons;
     }
 
-    public Message(String text, User user)
-    {
+    public Message(String text, User user) {
         this.text = text;
         this.id = user.getFullId();
     }
 
-    public Message(org.telegram.telegrambots.meta.api.objects.Message telegramMessage)
-    {
+    public Message(org.telegram.telegrambots.meta.api.objects.Message telegramMessage) {
 
         this.id = new Id(telegramMessage.getChatId(), PlatformType.Telegram);
         if (telegramMessage.hasText())
             this.text = telegramMessage.getText();
     }
 
-    public boolean hasPhoto()
-    {
-        return photoPath != null;
-    }
-
-    public boolean hasButtons()
-    {
+    public boolean hasButtons() {
         return buttons != null;
     }
 
-
-    public boolean hasText()
-    {
+    public boolean hasText() {
         return text != null;
     }
 
-    public User getUser()
-    {
+    public User getUser() {
         return UserDatabase.getUser(this.id);
-    }
-
-    public String getPhotoPath()
-    {
-        return photoPath;
-    }
-
-    public void setPhotoPath(String path)
-    {
-        this.photoPath = path;
     }
 }
